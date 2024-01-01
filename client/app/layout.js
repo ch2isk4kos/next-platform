1f10dfdd7f3b73aece8024d80345e60f3abfac0c
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-01 15:04:39 -0500

    undo provider changes

--- client/app/layout.js
+++ client/app/layout.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
