56b392990aff380287886217aedb61b75a62e363
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-01 15:05:02 -0500

    add components directory

---
+++ client/app/components/nav/NavbarMain.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavbarMain = () => {
+  return (
+    <div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      Navbar
+    </div>
+  );
+};
+
+export default NavbarMain;
