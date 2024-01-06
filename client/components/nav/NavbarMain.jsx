0d7d1d63580db2e287b273ae3497584654403ff9
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-06 12:39:14 -0500

    playing chess with conditional rendering of signin/up and user dashboard link(s)

--- client/components/nav/NavbarMain.jsx
+++ client/components/nav/NavbarMain.jsx
@@ -69,24 +69,22 @@ export default function NavbarMain() {
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">
-        {isLoaded && user && (
-          <>
-            <Link color="foreground" href="/dashboard">
-              Dashboard
-            </Link>
-            <UserButton afterSignOutUrl="/" />
-          </>
-        )}
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+      {isLoaded && user && (
+        <NavbarContent justify="end">
+          <Link color="foreground" href="/dashboard">
+            Dashboard
+          </Link>
+          <UserButton afterSignOutUrl="/" />
+        </NavbarContent>
+      )}
+      <NavbarItem className="hidden lg:flex">
+        <Link href="#">Login</Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Button as={Link} color="primary" href="" variant="flat">
+          Sign Up
+        </Button>
+      </NavbarItem>
 
       <NavbarMenu>
         {menuItems.map((item, index) => (
