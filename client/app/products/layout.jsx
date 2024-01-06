a7e7cc8cc07410e3a999436d743429d589506fe2
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-06 12:25:05 -0500

    add test code

--- client/app/products/layout.jsx
+++ client/app/products/layout.jsx
@@ -2,12 +2,15 @@
 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        {/* <SideNav /> */}
-        {/*  */}
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    <>
+      <h1>Products</h1>
+    </>
+    // <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    //   <div className="w-full flex-none md:w-64">
+    //     {/* <SideNav /> */}
+    //     {/*  */}
+    //   </div>
+    //   <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    // </div>
   );
 }
