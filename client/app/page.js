e7cd01b724b989395c0d0a8046380093dd9fdbe1
ch2isk4kos <ch2isk4kos@gmail.com>
2024-01-06 12:38:23 -0500

    redirect to dashboard if user is signed in

--- client/app/page.js
+++ client/app/page.js
@@ -1,4 +1,10 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { userId } = auth();
+  if (userId) redirect("/dashboard");
+
   return (
     <main className="">
       <h1>Welcome</h1>
